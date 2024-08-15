ec16d742d9d12e6eda117318e56409d468ea19ec
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-08-15 17:51:29 +0200

    add move find missing number exercice

---
+++ src/arrays/FindMissingNumber.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class FindMissingNumber {
+    private static int findMissingNumber(int[] arr) {
+        //according to our example our range here will be from 1 -> 10
+        int n = arr.length + 1;
+        //mathematical approach
+        int sum = n * (n + 1) / 2; //sum of table without missing number
+        for (int j : arr) {
+            sum -= j;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 6, 3, 1, 2, 9, 8, 7, 4};
+        DsaArrayUtil.printArray(arr);
+        int missingNum = findMissingNumber(arr);
+        System.out.println(missingNum);
+    }
+
+
+}
